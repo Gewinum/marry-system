10a13be9a43e689d6729f26c543c37c1959da479
Gewinum <gewinum@gmail.com>
2022-05-27 12:51:35 +0400

    #1 Make chat prefix optional

--- src/gewinum/marrysystem/MarrySystem.php
+++ src/gewinum/marrysystem/MarrySystem.php
@@ -3,19 +3,21 @@
 namespace gewinum\marrysystem;
 
 use gewinum\marrysystem\commands\MarryCommand;
+use gewinum\marrysystem\constants\SettingsConstants;
 use gewinum\marrysystem\providers\FamiliesProvider;
 use gewinum\marrysystem\providers\MessagesProvider;
+use gewinum\marrysystem\providers\SettingsProvider;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class MarrySystem extends PluginBase implements Listener
 {
     private static self $_instance;
 
+    private SettingsProvider $settingsProvider;
     private FamiliesProvider $familiesProvider;
     private MessagesProvider $messagesProvider;
 
@@ -25,6 +27,7 @@ class MarrySystem extends PluginBase implements Listener
 
         @mkdir($this->getDataFolder());
 
+        $this->settingsProvider = new SettingsProvider;
         $this->familiesProvider = new FamiliesProvider;
         $this->messagesProvider = new MessagesProvider;
 
@@ -38,6 +41,11 @@ class MarrySystem extends PluginBase implements Listener
     public static function getInstance() : self
     {
         return self::$_instance;
+    }
+
+    public function getSettingsProvider() : SettingsProvider
+    {
+        return $this->settingsProvider;
     }
 
     public function getFamiliesProvider(): FamiliesProvider
@@ -55,13 +63,17 @@ class MarrySystem extends PluginBase implements Listener
      */
     public function onPlayerChat(PlayerChatEvent $event)
     {
+        if (!$this->getSettingsProvider()->getSetting(SettingsConstants::CHAT_PREFIX_ENABLED)) {
+            return;
+        }
+
         if ($this->getFamiliesProvider()->getPlayerFamily($event->getPlayer()->getName()) === null) {
             return;
         }
 
         $currentFormat = $event->getFormat();
 
-        $event->setFormat(TextFormat::RED . "[❤]" . $currentFormat);
+        $event->setFormat($this->getMessagesProvider()->getMessage("PlayerChatPrefix", false) . $currentFormat);
     }
 
     public function onPlayerQuit(PlayerQuitEvent $event)
